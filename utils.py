7c1a66d0515a25b4a55d0066254c75689218b0a3
Girik Bajaj <girik.bajaj@leverageedu.com>
2025-07-28 00:45:33 +0530

    add printing utils

--- utils.py
+++ utils.py
@@ -2,3 +2,15 @@ import datetime
 
 def get_timestamp():
     return datetime.datetime.now().timestamp()
+
+
+def print_separator():
+    print("**********************")
+
+
+def print_header(*args):
+    print("")
+    print_separator()
+    print(*args)
+    print("")
+    print_separator()
